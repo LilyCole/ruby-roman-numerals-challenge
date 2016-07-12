17caca55080f24963ddb4d95d6e1aa4f4b0dd621
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-07-12 11:27:25 -0400

    Add solution code

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -3,7 +3,20 @@ def to_roman(num)
   legend = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1].zip(
     %w('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'))
   ######  legend == [[1000, 'M'], [900, 'CM'], [500, 'D'], ...]
-  # Your Code Here
+
+  # Input Validation
+  return nil unless ((num.is_a? Integer) || (num.is_a? Float)) && num >= 0
+  num = num.round
+
+  # Solving
+  result = ''
+  legend.each do |arabic, roman|
+    while (num >= arabic)
+      result += roman
+      num -= arabic
+    end
+  end
+  result
 end
 
 def to_arabic(numeral_string)
@@ -12,5 +25,20 @@ def to_arabic(numeral_string)
   ######  legend = [['M', 1000], ['CM', 900], ['D', 500], ...]
   legend_hash = Hash[legend]
   ######  legend_hash = {'M' => 1000, 'CM' => 900, ...}
-  # Your Code Here
+
+  # Input Validation
+  return nil unless numeral_string.is_a? String # Not a string
+  return nil if numeral_string =~ /(
+    (.)\2{3}                # More than three of any character
+    |[^MDCLXVI]                           # Illegal characters
+    |I[^XVI]|V[^I]|X[^CLXVI]|L[^XVI]       # Illegal sequences
+    |C[^MDCLXVI]|D[^MCLXVI]|M[^MDCLXVI]
+  )/x
+
+  # Solving
+  nums = numeral_string.chars.map { |char| legend_hash[char] }
+  nums.map.with_index do |num, i|
+    num *= -1 if (i < nums.length - 1) && num < nums[i + 1]
+    num
+  end.reduce(:+)
 end
